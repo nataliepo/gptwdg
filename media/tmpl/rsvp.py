a491685d249556c5483c9be1945d44123b76e034
nataliepo <npodrazik@gmail.com>
2013-03-10 16:07:07 -0400

    adding rsvp form to rsvp page

--- media/tmpl/rsvp.py
+++ media/tmpl/rsvp.py
@@ -1,9 +1,15 @@
 #encoding UTF-8 
 
 #def body
-<div class="hero-unit">
+<div class="row">
 
     <h1>RSVP</h1>
+    
+    <div class="rsvp-form">
+        <iframe src="https://docs.google.com/forms/d/1cOPuZl5qAGqO9RURCr8d6WtElD5dAMJp0tNmCVrN5OI/viewform?embedded=true"
+    width="500" height="500" frameborder="0" marginheight="0"
+        marginwidth="0">Loading...</iframe>
+    </div>
 
 
    
